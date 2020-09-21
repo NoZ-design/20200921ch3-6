27376f05f4361acc3deb4c72f8853844a1a61a91
NoZ-design <68496572+NoZ-design@users.noreply.github.com>
2020-09-21 16:14:33 +0800

    Fig_3.14

---
+++ Fig_3.14/檔案總管右方重新命名.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Fig_3._14
+{
+    class 檔案總管右方重新命名
+    {
+        static void Main(string[] args)
+        {
+            int Number1;
+            int Number2;
+            int Sum;
+
+            Console.Write("Enter the first number: ");
+            Number1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            Number2 = int.Parse(Console.ReadLine());
+
+            Sum = Number1 + Number2;
+
+            Console.WriteLine($"Sum is {Sum}");
+        }
+    }
+}
